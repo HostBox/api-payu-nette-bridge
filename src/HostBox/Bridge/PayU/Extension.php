fcd1a8d32219c8445f56f4ac9edb9dd4617b3077
EIFEL <czeifel@gmail.com>
2014-07-24 01:40:59 +0200

    Initial commit

---
+++ src/HostBox/Bridge/PayU/Extension.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HostBox\Bridge\PayU;
+
+use Nette\DI\CompilerExtension;
+use Nette\InvalidArgumentException;
+
+
+class Extension extends CompilerExtension {
+
+    /** @inheritdoc */
+    public function loadConfiguration() {
+        $container = $this->getContainerBuilder();
+        $config = $this->getConfig();
+
+
+        $params = array('posId', 'posAuthKey', 'key1', 'key2');
+
+        if ($diff = array_diff($params, array_keys($config))) {
+            throw new InvalidArgumentException('Missing configuration: ' . implode(', ', $diff));
+        }
+
+        $container->addDefinition($this->prefix('config'))
+            ->setClass('HostBox\Api\PayU\Config')
+            ->setArguments(array($config['posId'], $config['posAuthKey'], $config['key1'], $config['key2']));
+
+        $container->addDefinition($this->prefix('connection'))
+            ->setClass('HostBox\Api\PayU\Connection');
+
+        $container->addDefinition($this->prefix('payu'))
+            ->setClass('HostBox\Api\PayU\PayU');
+    }
+}
